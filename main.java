139f96d52374b4140b5898aea79488386b7ab7f8
fumiya123 <98727343+fumiya123@users.noreply.github.com>
2024-09-06 23:35:15 +0700

    Create main.java
    Tugas String Operation

---
+++ main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SentenceAnalysis {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // input kalimat
+        System.out.print("Masukkan kalimat = ");
+        String kalimat = scanner.nextLine();
+        
+        // hitung panjang kalimat termasuk spasi, menggunakan fungsi length()
+        int panjangKalimat = kalimat.length();
+        
+        // variabel untuk menampung jumlah huruf vokal dan konsonan
+        int jumlahVokal = 0;
+        int jumlahKonsonan = 0;
+        
+        // set huruf vokal
+        String vokal = "AEIOUaeiou";
+        
+        // perulangan sebanyak panjang kalimat
+        for (char ch : kalimat.toCharArray()) { // toCharArray() mengonversi string menjadi array dari karakter (char[]).
+            // memeriksa ch berisi huruf atau tidak
+            if (Character.isLetter(ch)) {
+                // memeriksa apakah ch memeiliki huruf yang sama dengan yang ada di variabel vokal
+                if (vokal.indexOf(ch) != -1) {
+                    jumlahVokal++; // jika iya tambah 1 untuk variabel jumlaVokal
+                } else {
+                    jumlahKonsonan++; // jika tidak tambah 1 untuk varabel jumlahKonsonan
+                }
+            }
+        }
+        
+        // hasil
+        System.out.println("Panjang kalimat = " + panjangKalimat);
+        System.out.println("Jumlah huruf vokal = " + jumlahVokal);
+        System.out.println("Jumlah huruf konsonan = " + jumlahKonsonan);
+        
+        scanner.close();
+    }
+}
